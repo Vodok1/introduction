45a461de148babba65ff85fefee9eaf96a9c51cc
Jakub Bauza <bulzor95@gmail.com>
2020-05-11 23:44:03 +0200

    bug fixes, mobile ver

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const myname = document.querySelectorAll("#myname path");
-for (let i = 0; i < myname.length; i++) {
-  console.log(`Litera ${i} is ${myname[i].getTotalLength()}`);
-}
+//const myname = document.querySelectorAll("#myname path");
+//for (let i = 0; i < myname.length; i++) {
+//  console.log(`Litera ${i} is ${myname[i].getTotalLength()}`);
+//}
+//window.onload = () => {
+  const gitHub = document.querySelector(".github");
+  gitHub.classList.add("active");
+};
